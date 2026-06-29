d3a4a4bebb698dbea19c10a54060591d124a2e6c
kalpitaa <mgkalpitaa@gmail.com>
2026-06-29 11:38:27 +0530

    fix: updated correct AuthContext casing in PrivateRoute

--- frontend-superadmin/src/components/PrivateRoute.jsx
+++ frontend-superadmin/src/components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "./context/Authcontext";
+import { useAuth } from "./context/AuthContext";
 
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
